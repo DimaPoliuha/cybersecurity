720b0c274400220005ae526c896e5f78ac75f865
Dmitry Poliuha <dm.poliuha@gmail.com>
2019-10-20 18:23:22 +0300

    implemented caesar cipher

--- app/ciphers/caesar_cipher.py
+++ app/ciphers/caesar_cipher.py
@@ -2,17 +2,27 @@ from ciphers.base_cipher import BaseCipher
 
 
 class CaesarCipher(BaseCipher):
-    def encrypt(self, text: str, key: int) -> str:
+    def encrypt(self, text: bytes, key: int) -> str:
         result_text = ""
         for symbol in text:
-            encrypted_symbol_code = ord(symbol) + key
+            encrypted_symbol_code = symbol + key
             if encrypted_symbol_code > self.UNICODE_COUNT or encrypted_symbol_code < 0:
                 encrypted_symbol_code = encrypted_symbol_code % self.UNICODE_COUNT
             result_text += chr(encrypted_symbol_code)
         return result_text
 
-    def decrypt(self, text: str, key: int):
-        pass
+    def decrypt(self, text: bytes, key: int):
+        result_text = ""
+        for symbol in text:
+            decrypted_symbol_code = symbol - key
+            if decrypted_symbol_code > self.UNICODE_COUNT or decrypted_symbol_code < 0:
+                decrypted_symbol_code = decrypted_symbol_code % self.UNICODE_COUNT
+            result_text += chr(decrypted_symbol_code)
+        return result_text
 
-    def brute_force(self, text: str):
-        pass
+    def brute_force(self, text: bytes):
+        result_text = ""
+        for key in range(self.UNICODE_COUNT):
+            result_text += self.decrypt(text, key)
+            result_text += "\n\n"
+        return result_text
--- app/main.py
+++ app/main.py
@@ -1,38 +1,52 @@
 import argparse
+from pathlib import Path
 
 from ciphers.caesar_cipher import CaesarCipher
 
 ciphers = {"caesar": CaesarCipher}
 
 
-def main(cipher_name: str, text: str, key: int):
+def main(cipher_name: str, process: str, key: int, input_file: Path, output_file: Path):
     cipher = ciphers.get(cipher_name)
-    # data_path.mkdir(mode=0o777, exist_ok=True)
+    with open(input_file, "rb") as f:
+        text = f.read()
 
     if cipher is None:
         raise Exception("No such cipher")
 
     cipher_obj = cipher()
-    result = cipher_obj.encrypt(text, key)
-    print(result.encode("utf-8"))
+    if process == "encrypt":
+        result = cipher_obj.encrypt(text, key)
+    elif process == "decrypt":
+        result = cipher_obj.decrypt(text, key)
+    elif process == "bruteforce":
+        result = cipher_obj.brute_force(text)
+    else:
+        raise Exception("No such process")
+
+    with open(output_file, "wb") as f:
+        f.write(result.encode("utf-8"))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Cybersecurity tool")
 
     parser.add_argument("--cipher", help="cipher name", required=True, type=str)
-    parser.add_argument("--text", help="text to encrypt", required=True, type=str)
-    parser.add_argument("--key", help="key", required=True, type=int)
-    # parser.add_argument(
-    #     "--data_path",
-    #     help="path to store bot data",
-    #     required=False,
-    #     default=Path("bot_data/"),
-    #     type=Path,
-    # )
+    parser.add_argument("--process", help="process", required=True, type=str)
+    parser.add_argument("--key", help="key", required=False, default=0, type=int)
+    parser.add_argument("--input_file", help="file with text", required=True, type=Path)
+    parser.add_argument(
+        "--output_file",
+        help="file to store result",
+        required=False,
+        default=Path("app/output.txt"),
+        type=Path,
+    )
     args = parser.parse_args()
     cipher = args.cipher
-    text = args.text
+    process = args.process
     key = args.key
+    input_file = args.input_file
+    output_file = args.output_file
 
-    main(cipher, text, key)
+    main(cipher, process, key, input_file, output_file)
