d66d77ceab844f0468e8b0a082a21288d6bfe792
Dmytro Poliuha <dmytro.poliuha@dataart.com>
2019-12-15 16:55:40 +0200

    lab3

---
+++ lab3_xor/cipher/xor_cipher.py
@@ -0,0 +1,26 @@
+import itertools
+from cipher.base_cipher import BaseCipher
+from random import randint
+
+
+class XORCipher(BaseCipher):
+    def _algorithm(self, text: str, key, process: str) -> str:
+        result_text = ""
+        keys = itertools.cycle(key)
+        for symbol, key_code in zip(text, keys):
+            if process == "encrypt":
+                symbol_code = ord(symbol) + key_code
+            elif process == "decrypt":
+                symbol_code = ord(symbol) - key_code
+            else:
+                raise Exception("No such process")
+            if symbol_code > self.UNICODE_COUNT or symbol_code < 0:
+                symbol_code = symbol_code % self.UNICODE_COUNT
+            result_text += chr(symbol_code)
+        return result_text
+
+    @classmethod
+    def generate_key(cls):
+        key = [randint(0, cls.UNICODE_COUNT) for _ in range(randint(100, 1000))]
+        key_f = "".join(map(chr, key)).encode('utf-16','surrogatepass')
+        return key, key_f
