52801e9a01b74b89ad09d07ca176724ebbce09ef
Dmytro Poliuha <dmytro.poliuha@dataart.com>
2019-12-15 21:55:35 +0200

    fix

--- lab3_xor/main.py
+++ lab3_xor/main.py
@@ -5,16 +5,17 @@ from cipher.xor_cipher import XORCipher
 
 
 def main(process: str, input_file: Path, output_file: Path, key_file: Path):
-    with open(input_file, "r") as f:
-        text = f.read()
-
     cipher_obj = XORCipher()
     if process == "encrypt":
+        with open(input_file, "r") as f:
+            text = f.read()
         key, key_f = XORCipher.generate_key()
         with open(key_file, "wb") as f:
             f.write(key_f)
         result = cipher_obj.encrypt(text, key)
     elif process == "decrypt":
+        with open(input_file, "rb") as f:
+            text = f.read().decode('utf-16', 'surrogatepass')
         with open(key_file, "rb") as f:
             key = f.read()
         key = (ord(symbol) for symbol in key.decode('utf-16', 'surrogatepass'))
@@ -22,8 +23,8 @@ def main(process: str, input_file: Path, output_file: Path, key_file: Path):
     else:
         raise Exception("No such process")
 
-    with open(output_file, "w") as f:
-        f.write(result)
+    with open(output_file, "wb") as f:
+        f.write(result.encode('utf-16', 'surrogatepass'))
 
 
 if __name__ == "__main__":
